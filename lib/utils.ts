2bfa4665ad55885420692973efc3e65f55d24e0f
terrence <53181321+terr171@users.noreply.github.com>
2025-05-08 19:22:58 +0900

    added utility function to randomly select a basic avatar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { userAvatarUrls } from "@/constants/constants";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
