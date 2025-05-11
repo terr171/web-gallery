ffc857152807bbce490f586bc30a23b9732f0b36
terrence <53181321+terr171@users.noreply.github.com>
2025-05-11 18:11:55 +0900

    Refactored session to a separate client component

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,112 +1,115 @@
+"use client";
 import React from "react";
-import Link from "next/link";
-
+import { signOut, useSession } from "next-auth/react";
 import {
-  NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-
 import { cn } from "@/lib/utils";
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { UserRole } from "@/database/schema";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDownIcon } from "lucide-react";
 
-const Header = async () => {
-  const session = await auth();
+const HeaderUser = () => {
+  const { data: session } = useSession();
+
   return (
-    <header className="fixed top-0 left-0 right-0 flex z-5 bg-white border-b items-center justify-between p-4 h-16">
-      <div className="flex items-center gap-6">
-        <Link href="/" className="">
-          <h1 className="text-xl font-bold">Web Gallery</h1>
-        </Link>
-        <NavigationMenu className="hidden md:flex">
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={cn(
-                  navigationMenuTriggerStyle(),
-                  "font-medium text-sm hover:underline",
-                )}
-              >
-                <Link href="/explore">Explore</Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
+    <>
+      <div className="hidden lg:flex flex-row justify-between items-center w-full">
+        {session?.user?.role === UserRole.Admin && (
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={cn(
+                navigationMenuTriggerStyle(),
+                "font-medium text-sm hover:underline",
+              )}
+            >
+              <Link href="/admin">Admin</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        )}
 
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                asChild
-                className={cn(
-                  navigationMenuTriggerStyle(),
-                  "font-medium text-sm hover:underline",
-                )}
-              >
-                <Link href="/featured">Featured</Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
+        {session?.user?.name ? (
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href={`/user/${session.user.name}`}>
+                {session.user.name}
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ) : (
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href="/login">Login</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        )}
 
-            {session?.user?.role === UserRole.Admin && (
-              <NavigationMenuItem>
-                <NavigationMenuLink
-                  asChild
-                  className={cn(
-                    navigationMenuTriggerStyle(),
-                    "font-medium text-sm hover:underline",
-                  )}
-                >
-                  <Link href="/admin">Admin</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            )}
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
-      <div className="flex items-center gap-2 flex-shrink-0 ml-4">
-        <NavigationMenu>
-          <NavigationMenuList>
-            {session?.user?.name ? (
-              <>
-                <NavigationMenuItem>
-                  <NavigationMenuLink
-                    asChild
-                    className={navigationMenuTriggerStyle()}
-                  >
-                    <Link href={`/user/${session.user.name}`}>
-                      {session.user.name}
-                    </Link>
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-              </>
-            ) : (
-              <NavigationMenuItem>
-                <NavigationMenuLink
-                  asChild
-                  className={navigationMenuTriggerStyle()}
-                >
-                  <Link href="/login">Login</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            )}
-          </NavigationMenuList>
-        </NavigationMenu>
         {session?.user?.name && (
-          <form
-            action={async () => {
-              "use server";
-              await signOut({ redirectTo: "/", redirect: true });
-            }}
+          <Button
+            type="button"
+            size="sm"
+            variant="outline"
+            onClick={() => signOut({ redirectTo: "/", redirect: true })}
           >
-            <Button type="submit" size="sm" variant="outline">
-              Logout
-            </Button>
-          </form>
+            Logout
+          </Button>
         )}
       </div>
-    </header>
+      <div className="flex lg:hidden w-full justify-end items-center">
+        {session?.user?.name ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                size="sm"
+                className="flex items-center gap-x-1"
+              >
+                {session.user.name}
+                <ChevronDownIcon className="h-4 w-4 opacity-70" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-56">
+              <DropdownMenuItem asChild>
+                <Link href={`/user/${session.user.name}`}>My Profile</Link>
+              </DropdownMenuItem>
+              {session?.user?.role === UserRole.Admin && (
+                <DropdownMenuItem asChild>
+                  <Link href="/admin">Admin</Link>
+                </DropdownMenuItem>
+              )}
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={() => signOut({ redirectTo: "/", redirect: true })}
+                className="text-red-500 focus:text-red-500 focus:bg-red-50 dark:focus:text-red-400 dark:focus:bg-red-900/50"
+              >
+                Sign Out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <Button asChild variant="outline" size="sm">
+            <Link href="/login">Login</Link>
+          </Button>
+        )}
+      </div>
+    </>
   );
 };
-
-export default Header;
+export default HeaderUser;
---
+++ components/layout/HeaderUser.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const HeaderUser = () => {
+  return <div>HeaderUser</div>;
+};
+export default HeaderUser;
