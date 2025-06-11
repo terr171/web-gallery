e290d9be762cec29aa96e1bbe176df5821d47512
terrence <53181321+terr171@users.noreply.github.com>
2025-06-11 11:11:53 +0900

    Style(ProjectModalPreview): Adjust spacing and alignment

--- features/project/components/project-modal/Preview.tsx
+++ features/project/components/project-modal/Preview.tsx
@@ -25,7 +25,7 @@ const Preview = ({ files }: ProjectPreviewProps) => {
   }, [files]);
 
   return (
-    <div className="border rounded-md p-1 max-h-[320px] h-full overflow-hidden">
+    <div className="border rounded-md pt-1 h-[320px] lg:h-full overflow-hidden">
       <div className="text-xs text-gray-500 border-b pb-1 px-2">Preview</div>
       <iframe
         srcDoc={iframeContent}
